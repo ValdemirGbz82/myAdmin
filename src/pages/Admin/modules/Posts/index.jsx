477837571c959796522c315d4ce6029652b80382
Valdemir Bezerra <120420266+ValdemirGbz82@users.noreply.github.com>
2023-09-27 21:16:01 -0300

    Add files via upload

---
+++ src/pages/Admin/modules/Posts/index.jsx
@@ -0,0 +1,40 @@
+import { Table } from "antd"
+import { useEffect } from "react"
+import { useState } from "react"
+
+export function Posts() {
+    const [posts, setPosts] = useState([])
+
+    const columns = [
+        {
+            title: 'ID',
+            dataIndex: 'id',
+            key: 'id',
+          },
+          {
+            title: 'Titulo',
+            dataIndex: 'title',
+            key: 'title',
+          },
+
+    ]
+
+    useEffect(()=>{
+        async function fetchPosts() {
+           const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+           const data = await response.json()
+
+           setPosts(data)
+        }
+
+        fetchPosts()
+
+    }, [])
+
+    return <div>
+        <h1>Posts...</h1>
+            <Table columns={columns} dataSource={posts}/>
+    </div>
+    
+
+}
